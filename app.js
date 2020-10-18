8d19483ea349e278ca752d6f91dd852909a7c644
«Pavel <Mr.Sawv@gmail.com>
2020-10-18 05:03:34 +0300

    poehtgrjf

--- app.js
+++ app.js
@@ -3,16 +3,17 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const path = require('path');
-const cardsUsersRoutes = require('./routes/index');
-
+const usersRoutes = require('./routes/users.js');
+const cardsRoutes = require('./routes/cards.js');
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', cardsUsersRoutes);
+app.use('/', usersRoutes);
+app.use('/', cardsRoutes);
 app.get('*', (req, res) => {
-  res.send({ message: 'Запрашиваемый ресурс не найден' }, 404);
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
